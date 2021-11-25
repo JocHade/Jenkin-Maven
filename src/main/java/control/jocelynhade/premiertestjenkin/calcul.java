abff91facf07f174a5e622bf17a5538b11a6dd76
JocHade <74924115+JocHade@users.noreply.github.com>
2021-11-25 15:57:03 -0500

    FirstInit

---
+++ src/main/java/control/jocelynhade/premiertestjenkin/calcul.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control.jocelynhade.premiertestjenkin;
+
+/**
+ *
+ * @author famil
+ */
+public class calcul {
+    
+    int val1, val2;
+    
+    public static int add(int val1, int val2){
+    
+    int res;
+    
+    res = val1+val2;
+ 
+    return res;
+}
+    
+    
+}
